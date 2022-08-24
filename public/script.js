3a7970746afa6cd48c26735958e0809d0e84161d
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-08-24 21:19:10 +0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -28,17 +28,19 @@ message.addEventListener('keypress', function(event) {
 });
 
 socket.on("chat", (data) => {
-    output.innerHTML += `<p><strong>${data.sender} <span class="date_left_name">${getTime()}</span> <br></strong> ${data.message}</p>`;
-    let prefix = {
-        youtube: "https://www.youtube.com/"
+    if (data.message.trim().length !== 0) {
+        output.innerHTML += `<p><strong>${data.sender} <span class="date_left_name">${getTime()}</span> <br></strong> ${data.message}</p>`;
+        let prefix = {
+            youtube: "https://www.youtube.com/"
+        }
+        let youtube_index = data.message.search(prefix.youtube)
+        if (data.message.charAt(youtube_index) !== ''){
+            console.log(data.message.search(prefix.youtube), data.message.split(" "));
+            let tag = data.message.split(" ")[data.message.search(prefix.youtube)].split("/")[3].replace("watch?v=", "")
+            output.innerHTML += `<p><iframe width="400" height="400" src="https://www.youtube.com/embed/${tag}" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>`;
+        }
+        appWindow.scrollBy(0, 100);
     }
-    let youtube_index = data.message.search(prefix.youtube)
-    if (data.message.charAt(youtube_index) !== ''){
-        console.log(data.message.search(prefix.youtube), data.message.split(" "));
-        let tag = data.message.split(" ")[data.message.search(prefix.youtube)].split("/")[3].replace("watch?v=", "")
-        output.innerHTML += `<p><iframe width="400" height="400" src="https://www.youtube.com/embed/${tag}" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>`;
-    }
-    appWindow.scrollBy(0, 100);
 })
 
 function getTime() {
